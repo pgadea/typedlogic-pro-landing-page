43ac78680a4d2db06230c55a1fe84b2363817f20
Pierre Gadea <pgadea7@gmail.com>
2021-04-11 20:23:38 -0500

    add field names to form

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -89,6 +89,7 @@ const ContactUs = props => {
                 return (
                   <div key={i}>
                     <textarea
+                      name={field.field_name}
                       required={field.required === "Yes"}
                       placeholder={field.field_name}
                     />
@@ -98,7 +99,7 @@ const ContactUs = props => {
                 return (
                   <div key={i}>
                     <input
-                      field={field.field_name}
+                      name={field.field_name}
                       placeholder={field.field_name}
                       required={field.required === "Yes"}
                       type={field.field_type}
